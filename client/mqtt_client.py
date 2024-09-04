a21e560595a5d3d2d847d1e260152173dec7ab74
THqqqqp <2472412112@qq.com>
2024-09-04 18:28:05 +0800

    配置MQTT连接

---
+++ client/mqtt_client.py
@@ -0,0 +1,34 @@
+import paho.mqtt.client as mqtt
+
+# MQTT服务器地址
+broker_address = "localhost"  # 替换为你的MQTT代理地址
+port = 1883  # MQTT代理的端口，默认为1883
+client_id = "python_mqtt_client"  # MQTT客户端的唯一ID，不冲突即可
+
+
+# 当连接到MQTT代理时调用的回调函数
+def on_connect(client, userdata, flags, reason_code, properties):
+    print(f"Connected with result code {reason_code}")
+    # 订阅主题
+    client.subscribe("test/myTestTopic")
+
+
+# 当接收到MQTT消息时调用的回调函数
+def on_message(client, userdata, msg):
+    temp, hum = msg.payload.decode('utf-8').split(",")
+    print(temp, hum)
+    print(f"接收到消息： '{msg.payload.decode('utf-8')}',主题： '{msg.topic}'")
+
+
+# 创建MQTT客户端实例
+client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+
+# 为客户端设置回调函数
+client.on_connect = on_connect
+client.on_message = on_message
+
+# 连接到MQTT代理
+client.connect(broker_address, port)
+
+# 开始网络循环，处理接收到的消息和重连
+client.loop_forever()
--- main.py
+++ main.py
@@ -1,33 +1,75 @@
+import time
+
+import network
 import uasyncio as asyncio
+from umqtt.simple import MQTTClient
 
 import demos.DHT as DHT
 import demos.MH as MH
 
 
-# 定义异步函数来读取 MH 传感器数据
+# 光敏
 async def read_mh():
     while True:
         MH.main()
         await asyncio.sleep(1)  # 使用 await asyncio.sleep 来替代 time.sleep
 
 
-# 定义异步函数来读取 DHT 传感器数据
+# 温湿度
 async def read_dht():
     while True:
         DHT.main()
         await asyncio.sleep(1)
 
 
+# 连接Wi-Fi
+def connect_wifi(ssid, password):
+    # 初始化 STA（Station）模式
+    wlan = network.WLAN(network.STA_IF)
+    wlan.active(True)
+
+    # 检查是否已经连接
+    if not wlan.isconnected():
+        print('Connecting to network...')
+        wlan.connect(ssid, password)
+
+        # 等待连接成功
+        while not wlan.isconnected():
+            print('Waiting for connection...')
+            time.sleep(1)
+
+    print('Network connected:', wlan.ifconfig())
+
+
+# 配置MQTT
+def config_mqtt(mqtt_server='192.168.1.102'):
+    client_id = 'esp32-client'
+    topic_pub = 'test/myTestTopic'
+
+    client = MQTTClient(client_id, mqtt_server)
+    client.connect()
+
+    while True:
+        msg = b'Hello from ESP32'
+        client.publish(topic_pub, msg)
+        print("Sent:", msg)
+        time.sleep(5)
+
+
 # 主函数
 async def main():
     print("system start...")
+
+    connect_wifi('INET', 'INet1529st')
+
+    config_mqtt('192.168.1.102')
 
     # 创建任务并发执行
     mh_task = asyncio.create_task(read_mh())
     dht_task = asyncio.create_task(read_dht())
 
-    await mh_task
-    await dht_task
+    # await mh_task
+    # await dht_task
 
 
 if __name__ == '__main__':
